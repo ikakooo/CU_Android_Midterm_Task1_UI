7ba0614c7283fa4fdbf3b93dd2bf819eb6d61c9b
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-10-27 22:32:36 +0400

    fdsdf

---
+++ app/src/main/java/com/example/cu_android_midterm_task1_ui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cu_android_midterm_task1_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
