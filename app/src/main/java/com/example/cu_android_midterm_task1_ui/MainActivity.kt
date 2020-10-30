0ad2c08429947faf01851da2dcf3f639a5eb0a72
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-10-31 00:37:39 +0400

    გამორჩენილი სნაქბარებიც დავამატე

--- app/src/main/java/com/example/cu_android_midterm_task1_ui/MainActivity.kt
+++ app/src/main/java/com/example/cu_android_midterm_task1_ui/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.cu_android_midterm_task1_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +13,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        init()
+        val downloadButtonID = findViewById<TextView>(R.id.Downloads_TextView_ID)
+        val ratingStarsButtonID = findViewById<TextView>(R.id.ratingStars_TextView_ID)
+        val personRatedTextViewID = findViewById<TextView>(R.id.personRated_TextView_ID)
+        val travelTextViewID = findViewById<TextView>(R.id.travel_TextView_ID)
+        val similarTextViewID = findViewById<TextView>(R.id.similar_TextView_ID)
+
+       ///////////////////////SnackBar-ების კლიკზე გამოჩენეა////////////////////////////////
+        downloadButtonID.apply {
+        setOnClickListener { view ->
+            Snackbar.make(view, "$text Billion Downloads ", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }}
+
+        ratingStarsButtonID.apply {
+            setOnClickListener { view ->
+                Snackbar.make(view, "$text/5 Stars from ${personRatedTextViewID.text} users ", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            }
+        }
+        travelTextViewID.setOnClickListener { view ->
+            Snackbar.make(view, "${travelString_TextView_ID.text} ", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        similarTextViewID.setOnClickListener { view ->
+            Snackbar.make(view, "Similar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        recyclerViewSetContent()
     }
 
-    private fun init() {
+    private fun recyclerViewSetContent() {
         RecyclerView_ID.apply {
             layoutManager =
                 LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
